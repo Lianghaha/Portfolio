bf4b5168fb0ef71a244179a125ccde9f06c745a7
Lianghaha <jialiang0601@gmail.com>
2020-11-19 00:29:09 -0500

    start creating portfolio

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { Home } from "./pages/Home/Home"
+import { PlayGround } from "./pages/PlayGround/PlayGround"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+   return (
+      <Router>
+         <Switch>
+            <Route path="/Home" exact render={() => <Home />} />
+            <Route path="/" exact render={() => <PlayGround />} />
+         </Switch>
+      </Router>
+   )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById("root"))
---
+++ src/pages/PlayGround/PlayGround.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export const PlayGround = () => {
+    return (
+        <div>PlayGround</div>
+    )
+}
