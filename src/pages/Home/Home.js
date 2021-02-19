2297001a08a3d17db2737cb8294c617ae720d9f0
unknown <jialiang0601@gmail.com>
2021-02-18 23:21:12 -0500

    played around with animation

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,7 +3,7 @@ import "./Home.css"
 import { gsap, Power3, Elastic, Back } from "gsap"
 import { Wave } from "./Wave/Wave"
 import { FloatItem } from "./FloatItem/FloatItem"
-import { Navbar } from "./Navbar/Navbar"
+import { Navbar } from "../../lib/components/Navbar/Navbar"
 import { Greeting } from "./Greeting/Greeting"
 import { Avatar } from "./Avatar/Avatar"
 
@@ -12,6 +12,7 @@ export const Home = () => {
       //Navbar animation
       gsap
          .timeline()
+         //Navbar
          .from(".js-navItem", {
             opacity: 0,
             y: 30,
@@ -20,37 +21,61 @@ export const Home = () => {
             duration: 1.6,
             ease: Power3.easeOut,
          })
-         //Greeting
+         //Ballon
          .from(
-            ".js-greeting-letter",
+            ".js-ballon",
             {
-               stagger: 0.1,
-               duration: 1,
                display: "none",
+               marginTop: 360,
+               duration: 2,
             },
-            "-=0.7"
+            "-=1.5"
          )
-         .from(".js-greeting-slogan", {
-            duration: 2,
-            opacity: 0,
-         })
-
-      gsap
-         .timeline()
-         .from("svg", {
-            opacity: 0,
-            x: 350,
-            rotation: 360,
-            transformOrigin: "50% 55%",
-            duration: 2,
-            ease: Back.easeOut.config(2.6),
-         })
+         //Avatar
+         .from(
+            "svg",
+            {
+               opacity: 0,
+               x: 550,
+               rotation: 360,
+               transformOrigin: "50% 55%",
+               duration: 2,
+               ease: Back.easeOut.config(2.6),
+            },
+            "-=1.5"
+         )
          .from("#Avataaar_2", {
             display: "none",
             y: 500,
             duration: 1.2,
             ease: Elastic.easeOut.config(0.5, 1),
          })
+         //
+         .from(
+            ".js-astronaut",
+            {
+               display: "none",
+               marginTop: -200,
+               duration: 3,
+            },
+            "-=2"
+         )
+         //Greeting
+         .from(
+            ".js-greeting-letter",
+            {
+               stagger: 0.1,
+               duration: 0.5,
+               display: "none",
+            },
+            "-=2"
+         )
+         .from(".js-greeting-slogan", {
+            duration: 0.5,
+            opacity: 0,
+         })
+
+      gsap.timeline()
    }, [])
 
    return (
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,40 +1,106 @@
 import React, { useEffect } from "react"
 import "./Projects.css"
 import tempImg from "../../media/image/testgif.gif"
+import { Navbar } from "../../lib/components/Navbar/Navbar"
+import { gsap } from "gsap"
 
 export const Projects = () => {
    useEffect(() => {
+      gsap.from(
+         ".card",
+         {
+            stagger: 0.3,
+            duration: 1.6,
+            opacity: 0,
+         },
+         "-=2"
+      )
       //Movement Animation to happen
-      const card = document.querySelector(".card")
-      const container = document.querySelector(".cardContainer")
+      const cards = document.querySelectorAll(".card")
+      const containers = document.querySelectorAll(".card-container")
 
-      //Moving Animation Event
-      container.addEventListener("mousemove", (e) => {
-         let x = card.getBoundingClientRect().left + card.clientWidth / 2
-         let y = card.getBoundingClientRect().top + card.clientHeight / 2
+      for (let i = 0; i < containers.length; i++) {
+         //Moving Animation Event
+         containers[i].addEventListener("mousemove", (e) => {
+            //card center x Axis
+            let x =
+               cards[i].getBoundingClientRect().left + cards[i].clientWidth / 2
 
-         let xDeg = (x - e.pageX) / 25
-         let yDeg = (e.pageY - y) / 25
-         card.style.transform = `rotateY(${xDeg}deg) rotateX(${yDeg}deg)`
-         // let xAxis = (window.innerWidth / 2 - e.pageX) / 25
-         // let yAxis = (e.pageY - window.innerHeight / 2) / 25
-         // card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
-      })
-      //Animate In
-      container.addEventListener("mouseenter", (e) => {
-         card.style.transition = "none"
-      })
-      //Animate Out
-      container.addEventListener("mouseleave", (e) => {
-         card.style.transition = "all 0.5s ease"
-         card.style.transform = `rotateY(0deg) rotateX(0deg)`
-      })
+            //card center y axis
+            let y =
+               cards[i].getBoundingClientRect().top + cards[i].clientHeight / 2
+
+            let xDeg = (x - e.pageX) / 30
+            let yDeg = (e.pageY - window.pageYOffset - y) / 20
+
+            cards[i].style.transform = `rotateY(${xDeg}deg) rotateX(${yDeg}deg)`
+            // let xAxis = (window.innerWidth / 2 - e.pageX) / 25
+            // let yAxis = (e.pageY - window.innerHeight / 2) / 25
+            // card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
+         })
+         //Animate In
+         containers[i].addEventListener("mouseenter", (e) => {
+            cards[i].style.transition = "none"
+         })
+         //Animate Out
+         containers[i].addEventListener("mouseleave", (e) => {
+            cards[i].style.transition = "all 0.5s ease"
+            cards[i].style.transform = `rotateY(0deg) rotateX(0deg)`
+         })
+      }
    }, [])
    return (
-      <div className="Projects">
-         <div className="cardContainer">
-            <div className="card">
-               <img src={tempImg} alt="" />
+      <div className="projects">
+         <Navbar />
+         <div className="content">
+            <div className="project-1">
+               <h1>Find The Prophets</h1>
+               <div className="display card-container">
+                  <div className="display-card card">
+                     <img src={tempImg} alt="" />
+                  </div>
+               </div>
+               <div className="description card-container">
+                  <div className="description-card card">
+                     <h2>Description</h2>
+                     <p>
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry. Lorem Ipsum has been the
+                        industry's standard dummy text ever since the 1500s,
+                        when an unknown printer took a galley of type and
+                        scrambled it to make a type specimen book. It has
+                        survived not only five centuries, but also the leap into
+                        electronic typesetting, remaining essentially unchanged.
+                        It was popularised in the 1960s with the release of
+                        Letraset sheets containing Lorem Ipsum passages, and
+                        more recently with desktop publishing software like
+                        Aldus PageMaker including versions of Lorem Ipsum.
+                     </p>
+                     <h2>Why I built this project?</h2>
+                     <p>
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry. Lorem Ipsum has been the
+                        industry's standard dummy text ever since the 1500s,
+                        when an unknown printer took a galley of type and
+                        scrambled it to make a type specimen book. It has
+                        survived not only five centuries, but also the leap into
+                        electronic typesetting, remaining essentially unchanged.
+                        It was popularised in the 1960s with the release of
+                        Letraset sheets containing Lorem Ipsum passages, and
+                        more recently with desktop publishing software like
+                        Aldus PageMaker including versions of Lorem Ipsum.
+                     </p>
+                  </div>
+               </div>
+               <div className="frontend card-container horizontal-card-container">
+                  <div className="frontend-card card horizontal-card"></div>
+               </div>
+               <div className="backend card-container horizontal-card-container">
+                  <div className="backend-card card horizontal-card"></div>
+               </div>
+               <div className="backend card-container horizontal-card-container">
+                  <div className="backend-card card horizontal-card"></div>
+               </div>
             </div>
          </div>
       </div>
