caf966a223454209c53dd79244a071186910159c
unknown <jialiang0601@gmail.com>
2021-02-20 02:46:42 -0500

    added images to Tech Used Section, successfully created first responsive animation gif

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,9 +1,23 @@
 import React, { useEffect, useCallback } from "react"
 import "./Projects.css"
-import tempImg from "../../media/image/Projects/testgif.gif"
 import { gsap } from "gsap"
 import { Navbar } from "../../lib/components/Navbar/Navbar"
 import { TechComponent } from "./TechComponent/TechComponent"
+// Images
+import desktop from "../../media/image/Projects/Desktop.gif"
+import reactIcon from "../../media/image/Projects/React.png"
+import jsIcon from "../../media/image/Projects/JavaScript.png"
+import htmlIcon from "../../media/image/Projects/HTML5.png"
+import cssIcon from "../../media/image/Projects/CSS3.png"
+import axiosIcon from "../../media/image/Projects/Axios.png"
+import nodeIcon from "../../media/image/Projects/Node.png"
+import expressIcon from "../../media/image/Projects/Express.png"
+import restAPIIcon from "../../media/image/Projects/RestAPI.png"
+import mySQLIcon from "../../media/image/Projects/MySQL.png"
+import gitIcon from "../../media/image/Projects/Git.png"
+import psIcon from "../../media/image/Projects/PS.png"
+import herokuIcon from "../../media/image/Projects/Heroku.png"
+import awsIcon from "../../media/image/Projects/AWS.png"
 
 export const Projects = () => {
    const animateCards = useCallback((container, card, xDivider, yDivider) => {
@@ -43,6 +57,14 @@ export const Projects = () => {
       //    opacity: 0,
       // })
 
+      // gsap.from(".tech-component", {
+      //    marginTop: -50,
+      //    marginLeft: -30,
+      //    stagger: 0.15,
+      //    duration: 1,
+      //    opacity: 0,
+      // })
+
       //Movement Animation to happen
       // const cards = document.querySelectorAll(".card")
       // const containers = document.querySelectorAll(".card-container")
@@ -68,12 +90,14 @@ export const Projects = () => {
          <Navbar />
          <div className="content">
             <div className="project-1">
-               <h1 className="js-project-slideIn">
+               <h1 className="project-title js-project-slideIn">
                   FindTheProphets<span>.com</span>
                </h1>
                <div className="display card-container vertical-card-container">
                   <div className="display-card card vertical-card js-project-slideIn">
-                     <img src={tempImg} alt="" />
+                     <div className="desktop">
+                        <img src={desktop} alt="" />
+                     </div>
                   </div>
                </div>
                <div className="description card-container vertical-card-container">
@@ -108,16 +132,101 @@ export const Projects = () => {
                      </p>
                   </div>
                </div>
+               <h2 className="project-subtitle js-project-slideIn">
+                  Technologies Used
+               </h2>
                <div className="frontend card-container horizontal-card-container">
                   <div className="frontend-card card horizontal-card js-project-slideIn">
-                     <TechComponent image="123" text="456" />
+                     <h3 className="horizontal-card-title">Front-End</h3>
+                     <TechComponent image={reactIcon} text="React.js" />
+                     <TechComponent image={jsIcon} text="JavaScript ES6+" />
+                     <TechComponent image={cssIcon} text="CSS3" />
+                     <TechComponent image={htmlIcon} text="HTML5" />
+                     <TechComponent image={axiosIcon} text="Axios" />
                   </div>
                </div>
                <div className="backend card-container horizontal-card-container">
-                  <div className="backend-card card horizontal-card js-project-slideIn"></div>
+                  <div className="backend-card card horizontal-card js-project-slideIn">
+                     <h3 className="horizontal-card-title">Back-End</h3>
+                     <TechComponent image={nodeIcon} text="Node.js" />
+                     <TechComponent image={expressIcon} text="Express" />
+                     <TechComponent image={mySQLIcon} text="MySQL" />
+                     <TechComponent image={restAPIIcon} text="RESTful API" />
+                  </div>
                </div>
                <div className="other card-container horizontal-card-container">
-                  <div className="other-card card horizontal-card js-project-slideIn"></div>
+                  <div className="other-card card horizontal-card js-project-slideIn">
+                     <h3 className="horizontal-card-title">Other</h3>
+                     <TechComponent image={gitIcon} text="Git" />
+                     <TechComponent image={psIcon} text="Photoshop" />
+                     <TechComponent image={herokuIcon} text="Heroku" />
+                     <TechComponent image={awsIcon} text="AWS" />
+                  </div>
+               </div>
+            </div>
+            <hr />
+            <div className="project-1">
+               <h1 className="project-title js-project-slideIn">
+                  FindTheProphets<span>.com</span>
+               </h1>
+               <div className="display card-container vertical-card-container">
+                  <div className="display-card card vertical-card js-project-slideIn"></div>
+               </div>
+               <div className="description card-container vertical-card-container">
+                  <div className="description-card card vertical-card js-project-slideIn">
+                     <h2>Description</h2>
+                     <p>
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry. Lorem Ipsum has been the
+                        industry's standard dummy text ever since the 1500s,
+                        when an unknown printer took a galley of type and
+                        scrambled it to make a type specimen book. It has
+                        survived not only five centuries, but also the leap into
+                        electronic typesetting, remaining essentially unchanged.
+                        It was popularised in the 1960s with the release of
+                        Letraset sheets containing Lorem Ipsum passages, and
+                        more recently with desktop publishing software like
+                        Aldus PageMaker including versions of Lorem Ipsum.
+                     </p>
+                     <h2>Why I built this project?</h2>
+                     <p>
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry. Lorem Ipsum has been the
+                        industry's standard dummy text ever since the 1500s,
+                        when an unknown printer took a galley of type and
+                        scrambled it to make a type specimen book. It has
+                        survived not only five centuries, but also the leap into
+                        electronic typesetting, remaining essentially unchanged.
+                        It was popularised in the 1960s with the release of
+                        Letraset sheets containing Lorem Ipsum passages, and
+                        more recently with desktop publishing software like
+                        Aldus PageMaker including versions of Lorem Ipsum.
+                     </p>
+                  </div>
+               </div>
+               <h2 className="project-subtitle js-project-slideIn">
+                  Technologies Used
+               </h2>
+               <div className="frontend card-container horizontal-card-container">
+                  <div className="frontend-card card horizontal-card js-project-slideIn">
+                     <h3 className="horizontal-card-title">Front-End</h3>
+                     <TechComponent image={reactIcon} text="React.js" />
+                     <TechComponent image={jsIcon} text="Javascript ES6+" />
+                     <TechComponent image={cssIcon} text="CSS3" />
+                     <TechComponent image={htmlIcon} text="HTML5" />
+                  </div>
+               </div>
+               <div className="backend card-container horizontal-card-container">
+                  <div className="backend-card card horizontal-card js-project-slideIn">
+                     <h3 className="horizontal-card-title">Back-End</h3>
+                     <TechComponent image={reactIcon} text="TESTESTEST" />
+                  </div>
+               </div>
+               <div className="other card-container horizontal-card-container">
+                  <div className="other-card card horizontal-card js-project-slideIn">
+                     <h3 className="horizontal-card-title">Other</h3>
+                     <TechComponent image={reactIcon} text="TESTTEST" />
+                  </div>
                </div>
             </div>
          </div>
--- src/pages/Projects/TechComponent/TechComponent.js
+++ src/pages/Projects/TechComponent/TechComponent.js
@@ -1,11 +1,18 @@
 import React, { useEffect } from "react"
 import "./TechComponent.css"
-// import { gsap } from "gsap"
+import { gsap } from "gsap"
 
 export const TechComponent = (props) => {
    const { image, text } = props
 
    useEffect(() => {}, [])
 
-   return <div className="tech-component"></div>
+   return (
+      <div className="tech-component">
+         <div className="image-container">
+            <img src={image ? image : ""} alt={text ? text : ""} />
+         </div>
+         <p>{text ? text : ""}</p>
+      </div>
+   )
 }
