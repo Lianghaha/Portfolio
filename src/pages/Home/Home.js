bd9d6ea149b2aa355b2c85fffe5f30d7292d674e
unknown <jialiang0601@gmail.com>
2021-02-22 22:26:19 -0500

    changed home greeting icon

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -16,100 +16,100 @@ export const Home = () => {
          let logoYCoord =
             logo.getBoundingClientRect().top + logo.clientHeight / 2
 
-         // gsap
-         //    .timeline()
-         //    .from(".nav-logo", {
-         //       x: window.innerWidth / 2 - logoXCoord,
-         //       y: window.innerHeight / 2 - logoYCoord,
-         //       scale: 2,
-         //       duration: 0.6,
-         //       ease: Power3.easeIn,
-         //    })
-         //    //Navbar
-         //    .from(
-         //       ".js-navItem",
-         //       {
-         //          opacity: 0,
-         //          y: 30,
-         //          x: -50,
-         //          stagger: 0.3,
-         //          duration: 1.6,
-         //          ease: Power3.easeOut,
-         //       },
-         //       "-=0"
-         //    )
-         //    //Ballon
-         //    .from(
-         //       ".js-ballon",
-         //       {
-         //          display: "none",
-         //          marginTop: 220,
-         //          duration: 2,
-         //          ease: Back.easeOut.config(3),
-         //       },
-         //       "-=1.5"
-         //    )
-         //    //Avatar
-         //    .from(
-         //       ".avatar-circle",
-         //       {
-         //          opacity: 0,
-         //          x: 550,
-         //          rotation: 360,
-         //          transformOrigin: "50% 55%",
-         //          duration: 1,
-         //          ease: Back.easeOut.config(2.6),
-         //       },
-         //       "-=1.5"
-         //    )
-         //    .from(
-         //       "#Avataaar_2",
-         //       {
-         //          display: "none",
-         //          y: 500,
-         //          duration: 1.2,
-         //          ease: Elastic.easeOut.config(0.5, 1),
-         //       },
-         //       "-=0.5"
-         //    )
-         //    //
-         //    .from(
-         //       ".js-astronaut",
-         //       {
-         //          display: "none",
-         //          marginTop: -200,
-         //          duration: 3,
-         //          ease: Elastic.easeOut,
-         //       },
-         //       "-=1.2"
-         //    )
-         //    //Greeting
-         //    .from(
-         //       ".js-greeting-letter",
-         //       {
-         //          stagger: 0.08,
-         //          duration: 1,
-         //          display: "none",
-         //       },
-         //       "-=2.5"
-         //    )
-         //    .from(
-         //       ".js-greeting-slogan",
-         //       {
-         //          y: 10,
-         //          duration: 0.2,
-         //          opacity: 0,
-         //       },
-         //       "-=0.5"
-         //    )
-         //    .from(
-         //       ".wave-container",
-         //       {
-         //          opacity: 0,
-         //          duration: 3,
-         //       },
-         //       "-=1.5"
-         //    )
+         gsap
+            .timeline()
+            .from(".nav-logo", {
+               x: window.innerWidth / 2 - logoXCoord,
+               y: window.innerHeight / 2 - logoYCoord,
+               scale: 2,
+               duration: 0.6,
+               ease: Power3.easeIn,
+            })
+            //Navbar
+            .from(
+               ".js-navItem",
+               {
+                  opacity: 0,
+                  y: 30,
+                  x: -50,
+                  stagger: 0.3,
+                  duration: 1.6,
+                  ease: Power3.easeOut,
+               },
+               "-=0"
+            )
+            //Ballon
+            .from(
+               ".js-ballon",
+               {
+                  display: "none",
+                  marginTop: 220,
+                  duration: 2,
+                  ease: Back.easeOut.config(3),
+               },
+               "-=1.5"
+            )
+            //Avatar
+            .from(
+               ".avatar-circle",
+               {
+                  opacity: 0,
+                  x: 550,
+                  rotation: 360,
+                  transformOrigin: "50% 55%",
+                  duration: 1,
+                  ease: Back.easeOut.config(2.6),
+               },
+               "-=1.5"
+            )
+            .from(
+               "#Avataaar_2",
+               {
+                  display: "none",
+                  y: 500,
+                  duration: 1.2,
+                  ease: Elastic.easeOut.config(0.5, 1),
+               },
+               "-=0.5"
+            )
+            //
+            .from(
+               ".js-astronaut",
+               {
+                  display: "none",
+                  marginTop: -200,
+                  duration: 3,
+                  ease: Elastic.easeOut,
+               },
+               "-=1.2"
+            )
+            //Greeting
+            .from(
+               ".js-greeting-letter",
+               {
+                  stagger: 0.08,
+                  duration: 1,
+                  display: "none",
+               },
+               "-=2.5"
+            )
+            .from(
+               ".js-greeting-slogan",
+               {
+                  y: 10,
+                  duration: 0.2,
+                  opacity: 0,
+               },
+               "-=0.5"
+            )
+            .from(
+               ".wave-container",
+               {
+                  opacity: 0,
+                  duration: 5,
+               },
+               "-=5"
+            )
       }
    }, [])
 
