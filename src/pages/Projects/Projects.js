874269dc6efccbf0001cb0e158f04336485122b8
unknown <jialiang0601@gmail.com>
2021-02-24 16:03:20 -0500

    finished project page basic layout

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -12,6 +12,7 @@ import jsIcon from "../../media/image/Projects/Stack-Icon/JavaScript.png"
 import htmlIcon from "../../media/image/Projects/Stack-Icon/HTML5.png"
 import cssIcon from "../../media/image/Projects/Stack-Icon/CSS3.png"
 import axiosIcon from "../../media/image/Projects/Stack-Icon/Axios.png"
+import reduxIcon from "../../media/image/Projects/Stack-Icon/Redux.png"
 import nodeIcon from "../../media/image/Projects/Stack-Icon/Node.png"
 import expressIcon from "../../media/image/Projects/Stack-Icon/Express.png"
 import restAPIIcon from "../../media/image/Projects/Stack-Icon/RestAPI.png"
@@ -20,6 +21,8 @@ import gitIcon from "../../media/image/Projects/Stack-Icon/Git.png"
 import psIcon from "../../media/image/Projects/Stack-Icon/PS.png"
 import herokuIcon from "../../media/image/Projects/Stack-Icon/Heroku.png"
 import awsIcon from "../../media/image/Projects/Stack-Icon/AWS.png"
+import gsapIcon from "../../media/image/Projects/Stack-Icon/GSAP.png"
+
 //React-Icon
 import { BiLinkExternal } from "react-icons/bi"
 
@@ -119,39 +122,41 @@ export const Projects = () => {
                   </a>
                </div>
 
-               {/* <div className="display card-container vertical-card-container">
-                  <div className="display-card card vertical-card js-project-slideIn">
-                  </div>
-               </div> */}
                <div className="description card-container vertical-card-container">
                   <div className="description-card card vertical-card js-project-slideIn">
-                     <h2>Description</h2>
+                     <h3>Description</h3>
                      <p>
-                        Lorem Ipsum is simply dummy text of the printing and
-                        typesetting industry. Lorem Ipsum has been the
-                        industry's standard dummy text ever since the 1500s,
-                        when an unknown printer took a galley of type and
-                        scrambled it to make a type specimen book. It has
-                        survived not only five centuries, but also the leap into
-                        electronic typesetting, remaining essentially unchanged.
-                        It was popularised in the 1960s with the release of
-                        Letraset sheets containing Lorem Ipsum passages, and
-                        more recently with desktop publishing software like
-                        Aldus PageMaker including versions of Lorem Ipsum.
+                        FindTheProphets.com is a rating website that can help
+                        the users find out the credibility of the predictors.
+                        Users can search, filter, and rate different
+                        predictions. The predictions' score will affect related
+                        predictors' score, which indicates how credible a
+                        predictor is.
                      </p>
-                     <h2>Why I built this project?</h2>
                      <p>
-                        Lorem Ipsum is simply dummy text of the printing and
-                        typesetting industry. Lorem Ipsum has been the
-                        industry's standard dummy text ever since the 1500s,
-                        when an unknown printer took a galley of type and
-                        scrambled it to make a type specimen book. It has
-                        survived not only five centuries, but also the leap into
-                        electronic typesetting, remaining essentially unchanged.
-                        It was popularised in the 1960s with the release of
-                        Letraset sheets containing Lorem Ipsum passages, and
-                        more recently with desktop publishing software like
-                        Aldus PageMaker including versions of Lorem Ipsum.
+                        Technically, it's a Full Stack React Web Application.
+                        The front-end, back-end and the database were all built
+                        by myself from scratch.
+                     </p>
+                  </div>
+               </div>
+
+               <div className="description card-container vertical-card-container">
+                  <div className="description-card card vertical-card js-project-slideIn">
+                     <h3>Why I built this project?</h3>
+                     <p>
+                        During the pandemic, I saw too many unreliable daunting
+                        predictions intended just to attract attention. So I
+                        thought it would be a good idea to build a website to
+                        counter irresponsible predictors.
+                     </p>
+                     <p>
+                        Technically, I wanted to polish my front-end development
+                        skills(CSS, React, JavaScript ES6+), and deepen my
+                        understanding of full stack development. After I built
+                        this project, I can confidently say that I am capable to
+                        build a good looking full stack web application just by
+                        myself.
                      </p>
                   </div>
                </div>
@@ -160,8 +165,9 @@ export const Projects = () => {
                </h2>
                <div className="frontend card-container horizontal-card-container">
                   <div className="frontend-card card horizontal-card js-project-slideIn">
-                     <h3 className="horizontal-card-title">Front-End</h3>
+                     <h3>Front-End</h3>
                      <TechComponent image={reactIcon} text="React.js" />
+                     <TechComponent image={reduxIcon} text="Redux" />
                      <TechComponent image={jsIcon} text="JavaScript ES6+" />
                      <TechComponent image={cssIcon} text="CSS3" />
                      <TechComponent image={htmlIcon} text="HTML5" />
@@ -170,7 +176,7 @@ export const Projects = () => {
                </div>
                <div className="backend card-container horizontal-card-container">
                   <div className="backend-card card horizontal-card js-project-slideIn">
-                     <h3 className="horizontal-card-title">Back-End</h3>
+                     <h3>Back-End</h3>
                      <TechComponent image={nodeIcon} text="Node.js" />
                      <TechComponent image={expressIcon} text="Express" />
                      <TechComponent image={mySQLIcon} text="MySQL" />
@@ -179,7 +185,7 @@ export const Projects = () => {
                </div>
                <div className="other card-container horizontal-card-container">
                   <div className="other-card card horizontal-card js-project-slideIn">
-                     <h3 className="horizontal-card-title">Other</h3>
+                     <h3>Other</h3>
                      <TechComponent image={gitIcon} text="Git" />
                      <TechComponent image={psIcon} text="Photoshop" />
                      <TechComponent image={herokuIcon} text="Heroku" />
@@ -187,7 +193,7 @@ export const Projects = () => {
                   </div>
                </div>
             </div>
-            {/* <hr /> */}
+            <hr />
             <div className="project-2 project-item">
                <div className="project-title-container js-project-slideIn">
                   <a
@@ -200,61 +206,58 @@ export const Projects = () => {
                      <BiLinkExternal />
                   </a>
                </div>
+               <div className="description card-container vertical-card-container">
+                  <div className="description-card card vertical-card js-project-slideIn">
+                     <h3>Description</h3>
+                     <p>
+                        FindTheProphets.com is a rating website that can help
+                        the users find out the credibility of the predictors.
+                        Users can search, filter, and rate different
+                        predictions. The predictions' score will affect related
+                        predictors' score, which indicates how credible a
+                        predictor is.
+                     </p>
+                     <p>
+                        Technically, it's a Full Stack React Web Application.
+                        The front-end, back-end and the database were all built
+                        by myself from scratch.
+                     </p>
+                  </div>
+               </div>
 
                <div className="description card-container vertical-card-container">
                   <div className="description-card card vertical-card js-project-slideIn">
-                     <h2>Description</h2>
+                     <h3>Why I built this project?</h3>
                      <p>
-                        Lorem Ipsum is simply dummy text of the printing and
-                        typesetting industry. Lorem Ipsum has been the
-                        industry's standard dummy text ever since the 1500s,
-                        when an unknown printer took a galley of type and
-                        scrambled it to make a type specimen book. It has
-                        survived not only five centuries, but also the leap into
-                        electronic typesetting, remaining essentially unchanged.
-                        It was popularised in the 1960s with the release of
-                        Letraset sheets containing Lorem Ipsum passages, and
-                        more recently with desktop publishing software like
-                        Aldus PageMaker including versions of Lorem Ipsum.
+                        During the pandemic, I saw too many unreliable daunting
+                        predictions intended just to attract attention. So I
+                        thought it would be a good idea to build a website to
+                        counter irresponsible predictors.
                      </p>
-                     <h2>Why I built this project?</h2>
                      <p>
-                        Lorem Ipsum is simply dummy text of the printing and
-                        typesetting industry. Lorem Ipsum has been the
-                        industry's standard dummy text ever since the 1500s,
-                        when an unknown printer took a galley of type and
-                        scrambled it to make a type specimen book. It has
-                        survived not only five centuries, but also the leap into
-                        electronic typesetting, remaining essentially unchanged.
-                        It was popularised in the 1960s with the release of
-                        Letraset sheets containing Lorem Ipsum passages, and
-                        more recently with desktop publishing software like
-                        Aldus PageMaker including versions of Lorem Ipsum.
+                        Technically, I wanted to polish my front-end development
+                        skills(CSS, React, JavaScript ES6+), and deepen my
+                        understanding of full stack development. After I built
+                        this project, I can confidently say that I am capable to
+                        build a good looking full stack web application just by
+                        myself.
                      </p>
                   </div>
                </div>
+
                <h2 className="project-subtitle js-project-slideIn">
                   Technologies Used
                </h2>
-               <div className="frontend card-container horizontal-card-container">
-                  <div className="frontend-card card horizontal-card js-project-slideIn">
-                     <h3 className="horizontal-card-title">Front-End</h3>
+               <div className="stack card-container vertical-card-container">
+                  <div className="stack-card card vertical-card js-project-slideIn">
                      <TechComponent image={reactIcon} text="React.js" />
-                     <TechComponent image={jsIcon} text="Javascript ES6+" />
+                     <TechComponent image={reduxIcon} text="Redux" />
+                     <TechComponent image={jsIcon} text="JavaScript ES6+" />
                      <TechComponent image={cssIcon} text="CSS3" />
                      <TechComponent image={htmlIcon} text="HTML5" />
-                  </div>
-               </div>
-               <div className="backend card-container horizontal-card-container">
-                  <div className="backend-card card horizontal-card js-project-slideIn">
-                     <h3 className="horizontal-card-title">Back-End</h3>
-                     <TechComponent image={reactIcon} text="TESTESTEST" />
-                  </div>
-               </div>
-               <div className="other card-container horizontal-card-container">
-                  <div className="other-card card horizontal-card js-project-slideIn">
-                     <h3 className="horizontal-card-title">Other</h3>
-                     <TechComponent image={reactIcon} text="TESTTEST" />
+                     <TechComponent image={gitIcon} text="Git" />
+                     <TechComponent image={psIcon} text="Photoshop" />
+                     <TechComponent image={gsapIcon} text="GSAP Animation" />
                   </div>
                </div>
             </div>
