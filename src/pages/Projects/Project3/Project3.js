e017d457831fc3c8fe30b68d9c98016ae988e3cf
unknown <jialiang0601@gmail.com>
2021-03-08 21:25:29 -0500

    seperated projct-3 to another file

---
+++ src/pages/Projects/Project3/Project3.js
@@ -0,0 +1,74 @@
+import React from "react"
+import "./Project3.css"
+import { FiGithub } from "react-icons/fi"
+
+export const Project3 = (props) => {
+   const folderIcon = props.folderIcon
+
+   return (
+      <div className="project-3 project-item">
+         <div className="project-info">
+            <div className="project-title-container js-project-slideIn">
+               <div className="project-title">
+                  <h1>
+                     Other <span>Projects</span>
+                  </h1>
+               </div>
+            </div>
+            <div className="square card-container horizontal-card-container">
+               <div className="square-card card horizontal-card js-project-slideIn">
+                  <div className="other-project-icons">
+                     <div className="other-project-folder">{folderIcon()}</div>
+                     <a
+                        className="other-project-github"
+                        href="https://github.com/Lianghaha/play-redux"
+                     >
+                        <FiGithub />
+                     </a>
+                  </div>
+                  <h3>play-redux</h3>
+                  <p>
+                     A simple web app built while I was learning Redux.
+                     Practiced integrating <span>Redux with React</span>, adding
+                     Redux <span>Middleware</span> and conducting
+                     <span> API Calls</span> while using Redux
+                  </p>
+                  <div className="space-keywords"></div>
+                  <div className="other-project-keywords">
+                     <p>Redux</p>
+                     <p>React</p>
+                     <p>JavaScript</p>
+                  </div>
+               </div>
+            </div>
+            <div className="square card-container horizontal-card-container">
+               <div className="square-card card horizontal-card js-project-slideIn">
+                  <div className="other-project-icons">
+                     <div className="other-project-folder">{folderIcon()}</div>
+                     <a
+                        className="other-project-github"
+                        href="https://github.com/Lianghaha/warehouse-management"
+                     >
+                        <FiGithub />
+                     </a>
+                  </div>
+                  <h3>warehouse-system</h3>
+                  <p>
+                     A <span>Java</span> program that guides workers and tracks
+                     the inventory in the warehouse. Practiced
+                     <span> Object-Oriented Programming</span>,
+                     <span> MVC model</span> and learned some very useful
+                     <span> Design Patterns</span>
+                  </p>
+                  <div className="space-keywords"></div>
+                  <div className="other-project-keywords">
+                     <p>Java</p>
+                     <p>Design Patterns</p>
+                     <p>OOP</p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   )
+}
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -3,52 +3,15 @@ import "./Projects.css"
 import { gsap, Power2 } from "gsap"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
 import { Navbar } from "../../lib/components/Navbar/Navbar"
-import { TechComponent } from "./TechComponent/TechComponent"
 import { Project1 } from "./Project1/Project1"
 import { Project2 } from "./Project2/Project2"
-
-// Icon
-// import reactIcon from "../../media/image/Projects/Stack-Icon/React.png"
-// import jsIcon from "../../media/image/Projects/Stack-Icon/JavaScript.png"
-// import htmlIcon from "../../media/image/Projects/Stack-Icon/HTML5.png"
-// import cssIcon from "../../media/image/Projects/Stack-Icon/CSS3.png"
-// import axiosIcon from "../../media/image/Projects/Stack-Icon/Axios.png"
-// import reduxIcon from "../../media/image/Projects/Stack-Icon/Redux.png"
-// import nodeIcon from "../../media/image/Projects/Stack-Icon/Node.png"
-// import expressIcon from "../../media/image/Projects/Stack-Icon/Express.png"
-// import restAPIIcon from "../../media/image/Projects/Stack-Icon/RestAPI.png"
-// import mySQLIcon from "../../media/image/Projects/Stack-Icon/MySQL.png"
-// import gitIcon from "../../media/image/Projects/Stack-Icon/Git.png"
-// import psIcon from "../../media/image/Projects/Stack-Icon/PS.png"
-// import herokuIcon from "../../media/image/Projects/Stack-Icon/Heroku.png"
-// import awsIcon from "../../media/image/Projects/Stack-Icon/AWS.png"
-// import gsapIcon from "../../media/image/Projects/Stack-Icon/GSAP.png"
-// import figmaIcon from "../../media/image/Projects/Stack-Icon/Figma.png"
-//React-Icon
+import { Project3 } from "./Project3/Project3"
+import { icons } from "./Stack-Icons-Resources"
+//React-Icons
 import { BiLinkExternal } from "react-icons/bi"
 import { HiOutlineFolder } from "react-icons/hi"
-import { FiGithub } from "react-icons/fi"
 //Goggle Analytic
 import ReactGa from "react-ga"
-import { icons } from "./Stack-Icons-Resources"
-// const icons = {
-//    reactIcon,
-//    jsIcon,
-//    htmlIcon,
-//    cssIcon,
-//    axiosIcon,
-//    reduxIcon,
-//    nodeIcon,
-//    expressIcon,
-//    restAPIIcon,
-//    mySQLIcon,
-//    gitIcon,
-//    psIcon,
-//    herokuIcon,
-//    awsIcon,
-//    gsapIcon,
-//    figmaIcon,
-// }
 
 //GSAP Scroll Trigger
 gsap.registerPlugin(ScrollTrigger)
@@ -228,145 +191,7 @@ export const Projects = () => {
          <div className="content">
             <Project1 icons={icons} titleIcon={BiLinkExternal} />
             <Project2 icons={icons} titleIcon={BiLinkExternal} />
-
-            {/* <div className="project-2 project-item">
-               <div className="project-info">
-                  <div className="project-title-container js-project-slideIn">
-                     <a
-                        className="project-title project-title-hover"
-                        href="https://lianghaha.github.io/Portfolio/#/"
-                     >
-                        <h1>
-                           <span>this.</span>Portfolio
-                        </h1>
-                        <BiLinkExternal />
-                     </a>
-                  </div>
-                  <div className="description card-container vertical-card-container">
-                     <div className="description-card card vertical-card js-project-slideIn">
-                        <h3>About this project</h3>
-                        <p>
-                           <span>General: </span>This is a Portfolio website
-                           where I can display my projects and other information
-                           in a more vivid way.
-                        </p>
-                        <p>
-                           <span>Technical: </span>It is a pure Front-End React
-                           Web Application that focuses more on animation and
-                           design.
-                        </p>
-                     </div>
-                  </div>
-                  <div className="description card-container vertical-card-container">
-                     <div className="description-card card vertical-card js-project-slideIn">
-                        <h3>Why I built this project?</h3>
-                        <p>
-                           <span>General: </span>I need a Portfolio website to
-                           show my projects and information. I could choose to
-                           build a quick and simple one, but I thought if I had
-                           to spend time to build one, why not make it more
-                           interesting?
-                        </p>
-                        <p>
-                           <span>Technical: </span>Building good-looking UI
-                           always makes me excited, and I am especially
-                           interested in animation. So I decided to learn more
-                           about animation and also some design tools
-                           (Photoshop, Figma) through building this website.
-                        </p>
-                     </div>
-                  </div>
-               </div>
-               <div className="project-stack">
-                  <h2 className="project-subtitle js-project-slideIn">
-                     Technologies Used
-                  </h2>
-                  <div className="stack card-container vertical-card-container">
-                     <div className="stack-card card vertical-card js-project-slideIn">
-                        <TechComponent image={reactIcon} text="React.js" />
-                        <TechComponent image={reduxIcon} text="Redux" />
-                        <TechComponent image={jsIcon} text="JavaScript ES6+" />
-                        <TechComponent image={cssIcon} text="CSS3" />
-                        <TechComponent image={htmlIcon} text="HTML5" />
-                        <TechComponent image={gitIcon} text="Git" />
-                        <TechComponent image={psIcon} text="Photoshop" />
-                        <TechComponent image={gsapIcon} text="GSAP Animation" />
-                        <TechComponent image={figmaIcon} text="Figma" />
-                     </div>
-                  </div>
-                  <div className="break-line js-project-slideIn"></div>
-               </div>
-            </div> */}
-
-            {/* <div className="project-3 project-item">
-               <div className="project-info">
-                  <div className="project-title-container js-project-slideIn">
-                     <div className="project-title">
-                        <h1>
-                           Other <span>Projects</span>
-                        </h1>
-                     </div>
-                  </div>
-                  <div className="square card-container horizontal-card-container">
-                     <div className="square-card card horizontal-card js-project-slideIn">
-                        <div className="other-project-icons">
-                           <div className="other-project-folder">
-                              <HiOutlineFolder />
-                           </div>
-                           <a
-                              className="other-project-github"
-                              href="https://github.com/Lianghaha/play-redux"
-                           >
-                              <FiGithub />
-                           </a>
-                        </div>
-                        <h3>play-redux</h3>
-                        <p>
-                           A simple web app built while I was learning Redux.
-                           Practiced integrating <span>Redux with React</span>,
-                           adding Redux <span>Middleware</span> and conducting
-                           <span> API Calls</span> while using Redux
-                        </p>
-                        <div className="space-keywords"></div>
-                        <div className="other-project-keywords">
-                           <p>Redux</p>
-                           <p>React</p>
-                           <p>JavaScript</p>
-                        </div>
-                     </div>
-                  </div>
-                  <div className="square card-container horizontal-card-container">
-                     <div className="square-card card horizontal-card js-project-slideIn">
-                        <div className="other-project-icons">
-                           <div className="other-project-folder">
-                              <HiOutlineFolder />
-                           </div>
-                           <a
-                              className="other-project-github"
-                              href="https://github.com/Lianghaha/warehouse-management"
-                           >
-                              <FiGithub />
-                           </a>
-                        </div>
-                        <h3>warehouse-system</h3>
-                        <p>
-                           A <span>Java</span> program that guides workers and
-                           tracks the inventory in the warehouse. Practiced
-                           <span> Object-Oriented Programming</span>,
-                           <span> MVC model</span> and learned some very useful
-                           <span> Design Patterns</span>
-                        </p>
-                        <div className="space-keywords"></div>
-                        <div className="other-project-keywords">
-                           <p>Java</p>
-                           <p>Design Patterns</p>
-                           <p>OOP</p>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
-          */}
+            <Project3 folderIcon={HiOutlineFolder} />
          </div>
       </div>
    )
